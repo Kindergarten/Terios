bfd2fcc280dcc8909f5e85cf47816dfd53d6aeac
AdamPoczatek <adam.poczatek@brandview.com>
2014-03-17 23:51:25 +0000

    Grunt.js first experiments Added node_modules to gitignore

---
+++ grunt/bundles.js
@@ -0,0 +1,10 @@
+module.exports = {
+    js: [
+        "./development/js/base.js",
+        "./development/js/utilities.js"
+    ],
+    css: [
+        "./public/css/base.css",
+        "./public/css/theme.css"
+    ]
+};
---
+++ grunt/scss.js
@@ -0,0 +1,8 @@
+/** List of all SCSS files to be compiled */
+
+module.exports = {
+    /** target: source */
+
+    "./public/css/base.css": "./development/sass/base.scss",
+    "./public/css/theme.css": "./development/sass/theme.scss"
+};
